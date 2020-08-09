f66c5d960a2d5de8c9243ecc24c616e73f924474
Maksim Vlasov <vlasov.maksim@hotmail.com>
2020-08-09 16:04:06 +0300

    Add an empty project

---
+++ src/main/kotlin/org/maks/scheduledmethodexample/ScheduledMethodExampleApplication.kt
@@ -0,0 +1,11 @@
+package org.maks.scheduledmethodexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScheduledMethodExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScheduledMethodExampleApplication>(*args)
+}
---
+++ src/test/kotlin/org/maks/scheduledmethodexample/ScheduledMethodExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.maks.scheduledmethodexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScheduledMethodExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
